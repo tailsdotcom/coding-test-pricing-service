3c8d97102045c8a735de9077f9243cd1d8eb0d25
hanteo <11653321+HanTeo@users.noreply.github.com>
2018-07-21 19:39:02 +0100

    marshall order object at endpoint

--- app/app/api/endpoints/pricing.py
+++ app/app/api/endpoints/pricing.py
@@ -2,7 +2,6 @@ from ...main import api
 from ...core.pricing import get_price_rules
 from flask_restplus import Resource
 
-
 ns = api.namespace('pricing', description='Pricing')
 
 
@@ -15,3 +14,46 @@ class PricingRules(Resource):
 
         return rules
 
+
+order = ns.schema_model('Order', {
+    'type': 'object',
+    'properties': {
+        'order': {
+            'type': 'object',
+            'properties': {
+                'id': {
+                    'type': 'integer',
+                },
+                'customer': {
+                    'type': 'object',
+                    'properties': {
+                        'name': {'type': 'string'},
+                        'customer_id': {'type': 'integer'}
+                    },
+                },
+                'items': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'product_id': {
+                                'type': 'integer'
+                            },
+                            'quantity': {
+                                'type': 'integer'
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
+
+
+@ns.route('/quote')
+class Quote(Resource):
+    @ns.expect(order, validate=True)
+    @ns.doc(description='Quote a given order')
+    def post(self):
+        return 'hello world'
